ff895c7fd1637b2a4c4a0dd246b1877662e56585
Stefan Dürrenberger <stduerre@greenpeace.org>
2019-05-07 17:22:43 +0200

    autoloader map update

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -8,5 +8,6 @@ $baseDir = dirname($vendorDir);
 return array(
     'Greenpeace\\Planet4GPCHBlocks\\Blocks\\Planet4_GPCH_Base_Block' => $baseDir . '/includes/blocks/Planet4_GPCH_Base_Block.php',
     'Greenpeace\\Planet4GPCHBlocks\\Blocks\\Planet4_GPCH_Block_Form_Progress_Bar' => $baseDir . '/includes/blocks/Planet4_GPCH_Block_Form_Progress_Bar.php',
+    'Greenpeace\\Planet4GPCHBlocks\\Blocks\\Planet4_GPCH_Block_Header_Image' => $baseDir . '/includes/blocks/Planet4_GPCH_Block_Header_Image.php',
     'Greenpeace\\Planet4GPCHBlocks\\Planet4_GPCH_Plugin_Blocks' => $baseDir . '/includes/Plugin.php',
 );
